4d0a7a1cdb937a9ccbb36dc546c8782277f697c7
Iru Cai <mytbk920423@gmail.com>
2013-01-05 22:56:02 +0800

    Modify tdm to be compliant with other distros.

--- tdm-0.2.3/install.sh
+++ tdm-0.2.3/install.sh
@@ -4,13 +4,10 @@ if [[ ! -n ${PREFIX} ]]; then
 	PREFIX="/usr/local"
 fi
 
-if [[ ! -n ${DESTDIR} ]]; then
-	DESTDIR="/"
-fi
-
 echo "install to ${PREFIX}"
-install tdm ${DESTDIR}/${PREFIX}/bin/tdm
+install tdm ${DESTDIR}${PREFIX}/bin/tdm
+install tdmctl ${DESTDIR}${PREFIX}/bin/tdmctl
 SED_COMMAND="/PREFIX=/{s#\$#${PREFIX}#g}"
-sed -i "${SED_COMMAND}" ${DESTDIR}/${PREFIX}/bin/tdm
-cp -Rv share ${DESTDIR}/${PREFIX}/
+sed -i "${SED_COMMAND}" ${DESTDIR}${PREFIX}/bin/tdm
+cp -Rv share ${DESTDIR}${PREFIX}/
 
